ecde797ad0ee1ff872602ef4f47746602fe42df1
j0seDav1 <jose.davi.andrade@escola.pr.gov.br>
2026-08-20 12:40:34 -0300

    diminuir senha

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto')
 numeroSenha.textContent = 5;
+
+tamanhoSenha = 8
+numeroSenha.textContent = tamanhoSenha
+
+const botoes =  document.querySelectorAll('.parametro-senha__texto')
+
+
+
+botoes[o].onclick = diminuir;
+
+function diminuir() {
+    tamanhoSenha--;
+    numeroSenha.textContent = tamanhoSenha
+}
