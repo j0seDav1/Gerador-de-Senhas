9a38a59da1afac2aaf79f7ff4ee06f0e136b3a31
j0seDav1 <jose.davi.andrade@escola.pr.gov.br>
2026-08-27 12:43:39 -0300

    s

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto')
 numeroSenha.textContent = 5;
+const campoSenha = document.querySelector('#campo-senha')
+
+campoSenha.value = 'Coloque a Senha'
+
+let letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+let letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz'
+let numeros = '1234567890'
 
 tamanhoSenha = 8
 numeroSenha.textContent = tamanhoSenha
 
-const botoes =  document.querySelectorAll('.parametro-senha__texto')
+const botoes =  document.querySelectorAll('.parametro-senha__botao')
 
 
 
-botoes[o].onclick = diminuir;
+botoes[0].onclick = diminuir;
 
 function diminuir() {
     tamanhoSenha--;
+    numeroSenha.textContent = tamanhoSenha
+}
+
+botoes[1].onclick = aumentar
+
+function aumentar() {
+    tamanhoSenha++;
     numeroSenha.textContent = tamanhoSenha
 }
